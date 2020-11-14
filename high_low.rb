4dd0f0fcc0e9d1b64770a10b3e489daa81857f52
Andrew Sloan <sloancodes@gmail.com>
2020-11-14 13:02:27 -0700

    getting the game going

--- high_low.rb
+++ high_low.rb
@@ -1,4 +1,6 @@
 require_relative 'deck.rb'
+require_relative 'menu.rb'
+require_relative 'wallet.rb'
 
 class HighLow
 
@@ -14,26 +16,86 @@ class HighLow
     # ask user if they want to bet again
     # if yes then loop back to the top else exit go back to menu/lobby
 
-    def initialize
-    end
-
+    
+    attr_accessor :money 
+    
     def display_game
-        puts "Welcome to High Low!"
+        puts "*******  Welcome to High Low!  *******"
+        puts "----------------------------------------------------------------"
+        puts "Here's the rules:"
+        puts "Place a bet. Dealer will show a card."
+        puts "Make a guess if the follwing dealer draw" 
+        puts "will be higher or lower than the first card presented."
+        puts "The dealer will then draw a card. If that card corresponds with"
+        puts "your guess, you win. If not, the dealer recieves your bet"
+        puts "-----------------------------------------------------------------"
+        make_sure
     end
-
-    def get_bet
-    puts "Place a bet"
-    @bet = gets.strip.to_i
-    end
-
-    def show_card_1
-    test_deck = Deck.new()
-    test_deck.random_card
-    end
-
- 
 
     
+    def make_sure
+        puts "Ready to play? y/n"
+        user_choice = gets.strip
+        if user_choice == "y"
+            get_bet
+        else
+            "Sending you back to the lobby"
+            lobby
+    end 
+    end
+    
+    def get_bet
+    puts "You current wallet total is:$ #{Wallet.new.money}. Place a bet."
+    choice = gets.strip.to_i
+    
 
+    show_card_1
+    end
+    
+    def show_card_1
+    test_deck = Deck.new()
+    puts test_deck.random_card
+    end
+    
+
+
+    def ask
+        puts "Will the next card be higher or lower than" #add card just shown
+        choice = gets.strip
+        if choice == "high"
+    end
+end
+        
+        def show_card_2
+        test_deck = Deck.new()
+        puts test_deck.random_card
+        if #the guess matches
+            win
+        else
+            lose
+    end
+end
+
+        
+        def win
+            puts "You doubled your bet!"
+        end
+
+        def lose
+            "YOU LOST"
+            play_again
+        end
+
+        def play_again
+            "Want to play again? y/n"
+            user_choice = gets.strip
+            if user_choice == "y"
+            get_bet
+            
+            
+        end
+    end
+    
+    
 
 end
