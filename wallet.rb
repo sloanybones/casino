8debe50ab0630993220845e7455a297feb667ee9
Joshua Parco <jacparco@gmail.com>
2020-11-16 09:29:51 -0700

    updated wallet

--- wallet.rb
+++ wallet.rb
@@ -13,10 +13,7 @@ class Wallet
     end
 
     def current_balance
-<<<<<<< HEAD
-=======
         # puts "$#{@money}"
->>>>>>> b09246c6381515b9745d0c3905fde9af6142688d
         @money
     end
 
@@ -27,9 +24,4 @@ class Wallet
             false
         end
     end
-<<<<<<< HEAD
-    
 end
-=======
-end
->>>>>>> ce5864d27d9c934780a7bc3706ef127f51ffb287
